02fda7b0457e98c0e81ffa93f8702a577375547a
Arun Ganesan <arunganesan123@gmail.com>
2019-01-10 15:47:51 -0500

    Automatically generated starter module for watchfon_gear

---
+++ watchfon_gear/src/main/java/edu/umich/carlab/watchfon_gear/MiddlewareImpl.java
@@ -0,0 +1,31 @@
+package edu.umich.carlab.watchfon_gear;
+
+import edu.umich.carlab.DataMarshal;
+import edu.umich.carlab.loadable.Middleware;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MiddlewareImpl implements Middleware {
+    public final static String APP = "watchfon_gear";
+    public final static String SPEED = "speed";
+
+    @Override
+    public String getName() {
+        return APP;
+    }
+
+    /**
+     * This function splits an grouped together sensor source to it's complementary parts
+     * This middleware doesn't need to split so this is the default case.
+     * @param dataObject
+     * @return
+     */
+    public Map<String, Float> splitValues(DataMarshal.DataObject dataObject) {
+        Map<String, Float> splitMap = new HashMap<>();
+        String device = dataObject.device;
+        if (!device.equals(APP)) return null;
+        splitMap.put(dataObject.sensor, dataObject.value[0]);
+        return splitMap;
+    }
+}
