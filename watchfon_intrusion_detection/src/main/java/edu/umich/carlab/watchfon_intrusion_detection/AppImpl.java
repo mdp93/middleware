c35639847f7584f2611a4d3042290631d5220422
Arun Ganesan <arunganesan123@gmail.com>
2019-01-11 13:01:14 -0500

    Automatically generated starter module for watchfon_intrusion_detection

---
+++ watchfon_intrusion_detection/src/main/java/edu/umich/carlab/watchfon_intrusion_detection/AppImpl.java
@@ -0,0 +1,34 @@
+package edu.umich.carlab.watchfon_intrusion_detection;
+
+import android.content.Context;
+import android.hardware.SensorManager;
+import android.util.Pair;
+import edu.umich.carlab.CLDataProvider;
+import edu.umich.carlab.DataMarshal;
+import edu.umich.carlab.loadable.App;
+import edu.umich.carlab.sensors.PhoneSensors;
+
+import java.util.Map;
+
+
+public class AppImpl extends App {
+    final String TAG = "watchfon_intrusion_detection";
+
+    public AppImpl(CLDataProvider cl, Context context) {
+        super(cl, context);
+        name = "watchfon_intrusion_detection";
+        sensors.add(new Pair<>(PhoneSensors.DEVICE, PhoneSensors.GPS));
+    }
+
+
+    @Override
+    public void newData(DataMarshal.DataObject dObject) {
+        if (dObject.dataType != DataMarshal.MessageType.DATA) return;
+        if (dObject.device.equals(MiddlewareImpl.APP)) return;
+        if (dObject.value == null) return;
+
+        Map<String, Float> gpsSplit = PhoneSensors.splitValues(dObject);
+        Float speed = gpsSplit.get(PhoneSensors.GPS_SPEED);
+        outputData(MiddlewareImpl.APP, dObject, MiddlewareImpl.SPEED, speed);
+    }
+}
---
+++ watchfon_intrusion_detection/src/main/java/edu/umich/carlab/watchfon_intrusion_detection/MiddlewareImpl.java
@@ -0,0 +1,17 @@
+package edu.umich.carlab.watchfon_intrusion_detection;
+
+import edu.umich.carlab.DataMarshal;
+import edu.umich.carlab.loadable.Middleware;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MiddlewareImpl extends Middleware {
+    public final static String APP = "watchfon_intrusion_detection";
+    public final static String SPEED = "speed";
+
+    @Override
+    public String getName() {
+        return APP;
+    }
+}
