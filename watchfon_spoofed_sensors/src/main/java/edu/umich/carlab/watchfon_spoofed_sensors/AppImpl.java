a351a0e7b2d1f19ec8fb2892df8b497e4b872070
Arun Ganesan <arunganesan123@gmail.com>
2019-01-11 12:58:40 -0500

    Wired basic watchfon spoofer middleware

--- watchfon_spoofed_sensors/src/main/java/edu/umich/carlab/watchfon_spoofed_sensors/AppImpl.java
+++ watchfon_spoofed_sensors/src/main/java/edu/umich/carlab/watchfon_spoofed_sensors/AppImpl.java
@@ -6,18 +6,34 @@ import android.util.Pair;
 import edu.umich.carlab.CLDataProvider;
 import edu.umich.carlab.DataMarshal;
 import edu.umich.carlab.loadable.App;
+import edu.umich.carlab.sensors.OpenXcSensors;
 import edu.umich.carlab.sensors.PhoneSensors;
 
 import java.util.Map;
 
+/**
+ * The watchfon spoof middleware takes the OpenXC sensors as input, and injects false noise based on a triggering
+ * condition. The output of this middleware is the injected data and a mask to specify the magnitude of the injection.
+ * This is used in the WatchFon project to detect injection into the CAN bus by comparing with the smartphone-derived
+ * sensors.
+ *
+ * The nature of the injection is controlled using this middleware's settings.
+ */
 
 public class AppImpl extends App {
     final String TAG = "watchfon_spoofed_sensors";
 
+    Double injectionMagnitude = 10d, newValue;
+
     public AppImpl(CLDataProvider cl, Context context) {
         super(cl, context);
         name = "watchfon_spoofed_sensors";
-        sensors.add(new Pair<>(PhoneSensors.DEVICE, PhoneSensors.GPS));
+        sensors.add(new Pair<>(OpenXcSensors.DEVICE, OpenXcSensors.SPEED));
+        sensors.add(new Pair<>(OpenXcSensors.DEVICE, OpenXcSensors.STEERING));
+        sensors.add(new Pair<>(OpenXcSensors.DEVICE, OpenXcSensors.FUEL));
+        sensors.add(new Pair<>(OpenXcSensors.DEVICE, OpenXcSensors.ODOMETER));
+        sensors.add(new Pair<>(OpenXcSensors.DEVICE, OpenXcSensors.ENGINERPM));
+        sensors.add(new Pair<>(OpenXcSensors.DEVICE, OpenXcSensors.GEAR));
     }
 
 
@@ -27,8 +43,11 @@ public class AppImpl extends App {
         if (dObject.device.equals(MiddlewareImpl.APP)) return;
         if (dObject.value == null) return;
 
-        Map<String, Float> gpsSplit = PhoneSensors.splitValues(dObject);
-        Float speed = gpsSplit.get(PhoneSensors.GPS_SPEED);
-        outputData(MiddlewareImpl.APP, dObject, MiddlewareImpl.SPEED, speed);
+        newValue = dObject.value[0] + injectionMagnitude;
+        outputData(MiddlewareImpl.APP, dObject, dObject.sensor, new Float[]{
+                newValue.floatValue(),
+                injectionMagnitude.floatValue(),
+            }
+        );
     }
 }
--- watchfon_spoofed_sensors/src/main/java/edu/umich/carlab/watchfon_spoofed_sensors/MiddlewareImpl.java
+++ watchfon_spoofed_sensors/src/main/java/edu/umich/carlab/watchfon_spoofed_sensors/MiddlewareImpl.java
@@ -9,9 +9,24 @@ import java.util.Map;
 public class MiddlewareImpl extends Middleware {
     public final static String APP = "watchfon_spoofed_sensors";
     public final static String SPEED = "speed";
+    public final static String STEERING = "steering";
+    public final static String ENGINERPM = "engine_rpm";
+    public final static String ODOMETER = "odometer";
+    public final static String FUEL = "fuel";
+    public final static String GEAR = "gear";
+
+    public final static String INJECTION_MAGNITUDE = "injection";
 
     @Override
     public String getName() {
         return APP;
+    }
+
+    @Override
+    public Map<String, Float> splitValues(DataMarshal.DataObject dataObject) {
+        Map<String, Float> splitMap = new HashMap<>();
+        splitMap.put(dataObject.sensor, dataObject.value[0]);
+        splitMap.put(INJECTION_MAGNITUDE, dataObject.value[1]);
+        return splitMap;
     }
 }
