84a2c6412d28b4170ceb0eed18fdae2ba0f52890
Arun Ganesan <arunganesan123@gmail.com>
2019-02-20 13:53:36 -0500

    [watchfon_gear] Working tensor flow lite gear prediction, and one hot decoding

--- watchfon_gear/src/main/java/edu/umich/carlab/watchfon_gear/AppImpl.java
+++ watchfon_gear/src/main/java/edu/umich/carlab/watchfon_gear/AppImpl.java
@@ -13,6 +13,7 @@ import org.tensorflow.lite.Interpreter;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
@@ -25,11 +26,15 @@ public class AppImpl extends SensorListAppBase {
 
     Interpreter tflite;
 
+    // It's a 10-sized one-hot encoding
+    float[][] labelProb = new float[1][10];
+    boolean runningPrediction = false;
 
     public AppImpl(CLDataProvider cl, Context context) {
         super(cl, context);
         name = "watchfon_gear";
         middlewareName = MiddlewareImpl.APP;
+        enableHistoricalLogging = true;
 
         if (context != null) {
             String vehicleName = middleware.getParameterOrDefault(context, middleware.VEHICLE_NAME, "");
@@ -47,6 +52,7 @@ public class AppImpl extends SensorListAppBase {
             }
         }
 
+        // In kilometers per hour
         subscribe(watchfon_speed.APP, watchfon_speed.SPEED);
     }
 
@@ -70,24 +76,67 @@ public class AppImpl extends SensorListAppBase {
         if (dObject.device.equals(MiddlewareImpl.APP)) return;
         if (dObject.value == null) return;
 
-        // TODO. For now we just output a placeholder.
-        outputData(MiddlewareImpl.APP, dObject, MiddlewareImpl.GEAR, 1f);
+        String dev, sen;
+        dev = dObject.device;
+        sen = dObject.sensor;
+        if (dev.equals(watchfon_speed.APP) && sen.equals(watchfon_speed.SPEED)) {
+            // 1. Check if the model is loaded. If not, return. We should have already loaded it.
+            if (tflite == null) {
+                Log.e(TAG, "Tensor Flow Lite model not initialized");
+                return;
+            }
 
-        // TODO. Since the data comes at a fast rate, we can't overload the prediction algorithm. Check if there is an ongoing prediction and return early if there is.
+            if (runningPrediction) {
+                Log.v(TAG, "Busy predicting previous value");
+                return;
+            }
 
-        // 1. Check if the model is loaded. If not, return. We should have already loaded it.
-        // 2. Get the speed samples at the last [-4, -3, -2, -1, 0] seconds (similar to getLatestData(dev, sen))
-            /*
-            In general, this API can be:
+            // 2. Get the speed samples at the last [-4, -3, -2, -1, 0] seconds (similar to getLatestData(dev, sen))
+            DataSample f1 = getDataAt(watchfon_speed.APP, watchfon_speed.SPEED, 4000L);
+            DataSample f2 = getDataAt(watchfon_speed.APP, watchfon_speed.SPEED, 3000L);
+            DataSample f3 = getDataAt(watchfon_speed.APP, watchfon_speed.SPEED, 2000L);
+            DataSample f4 = getDataAt(watchfon_speed.APP, watchfon_speed.SPEED, 1000L);
+            DataSample f5 = getDataAt(watchfon_speed.APP, watchfon_speed.SPEED, 0L);
 
-            getDataAt(dev, sen, -3.5):
-                It returns the data point that is closest to that time offset from now.
-                It returns the actual data (float value) and the time offset to that time (as close as it can)
+            if (f1 == null || f2 == null || f3 == null || f4 == null || f5 == null) {
+                Log.e(TAG, "Couldn't construct feature vector");
+                return;
+            }
 
-            getEstimateDataAt(dev, sen, -3.5):
-                Returns data point interpolated to that exact time
-                Returns distance to the nearest data point (a proxy for confidence in our return value)
-            */
-        // 3. Use feature set to make prediction
+            ByteBuffer bb = ByteBuffer.allocate(4*5);
+            bb.putFloat(f1.value);
+            bb.putFloat(f2.value);
+            bb.putFloat(f3.value);
+            bb.putFloat(f4.value);
+            bb.putFloat(f5.value);
+
+
+            // 3. Use feature set to make prediction
+            runningPrediction = true;
+            tflite.run(bb, labelProb);
+            runningPrediction = false;
+
+            // 4. Use reverse one-hot encoding to output the gear value
+            float gearValue = oneHotDecode(labelProb);
+            outputData(middleware.APP, middleware.GEAR, gearValue);
+        }
+    }
+
+
+    Integer oneHotDecode (float[][] labelProb) {
+        float maxVal = labelProb[0][0];
+        int maxIdx = 0;
+        float val;
+
+        for (int i = 1; i < labelProb[0].length; i++) {
+            val = labelProb[0][i];
+            if (val > maxVal) {
+                maxVal = val;
+                maxIdx = i;
+            }
+        }
+
+        // One hot encoding basically shifts the gear values over.
+        return maxIdx - 1;
     }
 }
